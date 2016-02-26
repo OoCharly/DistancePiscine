24f4249b7d899f60f6c25277ac183ec70e5317cb
OoCharly <OoCharly@users.noreply.github.com>
2016-02-26 19:15:38 +0100

    .

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: cdesvern <cdesvern@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/25 17:13:20 by cdesvern          #+#    #+#             */
-/*   Updated: 2016/02/26 18:17:43 by cdesvern         ###   ########.fr       */
+/*   Updated: 2016/02/26 19:09:38 by cdesvern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,6 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	out = ft_strnew(ft_strlen(s1) + ft_strlen(s2) + 1);
 	if (!(out && s1 && s2))
 		return (NULL);
-	ft_strcpy((char*)out, ft_strcat((char*)s1, s2));
+	ft_strcpy((char*)out, ft_strcat(ft_strcat(out, s1),s2));
 	return (out);
 }
--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: cdesvern <cdesvern@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/25 13:21:03 by cdesvern          #+#    #+#             */
-/*   Updated: 2016/02/26 17:39:34 by cdesvern         ###   ########.fr       */
+/*   Updated: 2016/02/26 19:10:13 by cdesvern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	len;
 	size_t	i;
 
+	if (!(dst && src))
+		return (0);
 	i = 0;
 	len = ft_strlen(dst);
 	while (i < (size - 1))
